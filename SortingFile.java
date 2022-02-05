05fcf1d5f2ceecd4b73ac337e5cf27cf0b071836
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-02-05 08:14:17 -0600

    agregar merge sort

--- SortingFile.java
+++ SortingFile.java
@@ -32,6 +32,8 @@ public class SortingFile{
     /** 
      * Metodo para sortear con el tipo "merge sort"
      * Obtenido y modificado a partir de el archivo "The Sound of Sorting Algorithm Cheat Sheet"
+     * @param lo elemento inicial
+     * @param hi ultimo elemento del array
      */
     public void mergeSort (int[] list,int lo, int hi){
         if (hi-lo<=1){
@@ -40,9 +42,43 @@ public class SortingFile{
         int mid = (hi+lo)/2;
         mergeSort(list,lo,mid);
         mergeSort(list,mid+1,hi);
-        int n1 = mid-lo +1;
-        int n2 = hi - mid;
         
+        int n1 = mid-lo +1; 
+        int n2 = hi - mid;
+
+        int left[] = new int[n1]; //crear arrays temporales
+        int right[] = new int[n2];
+        //copiar los objetos originales
+        for (int i=0;i<n1;i++){
+            left[i] = list[lo+i];
+        }
+        for (int j=0;j<n2;j++){
+            right[j] = list[mid + 1 + j];
+        }
+        //unir
+        int i = 0;
+        int j = 0;
+        int k = 1;
+        while (i<n1 && j<n2){
+            if (left[i]<=right[j]){
+                list[k]=left[i];
+                i++;
+            } else {
+                list[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<n1){
+            list[k]=left[i];
+            i++;
+            k++;
+        }
+        while (j<n2){
+            list[k] = right[j];
+            j++;
+            k++;
+        }
 
     }
 
