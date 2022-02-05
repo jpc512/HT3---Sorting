c00291106c274c1e3df7132478f00cba50829c48
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-02-05 15:28:26 -0600

    agregar selectionSort

--- SortingFile.java
+++ SortingFile.java
@@ -114,7 +114,18 @@ public class SortingFile{
      * @param list array con los elementos a ordenar
      */
     public void selectionSort (int[] list){
-
+        for(int i=0;i<list.length-1;i++){
+            int min = i;
+            for (int j=i+1;j<list.length;j++){
+                if (list[j]<list[min]){
+                    min = j;
+                }
+            }
+            //swap
+            int temp = list[i];
+            list[i] = list[min];
+            list[min] = temp;
+        }
     }
 
 
